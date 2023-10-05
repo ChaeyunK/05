b9db9fee3e1dfe9329a54b79adbe506ffdebeed7
ChaeyunK <iehate52@gmail.com>
2023-10-05 17:05:36 +0900

    6

--- main.c
+++ main.c
@@ -4,21 +4,30 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-		char c;
-		int num=0;
+		int answer = 198;
+		int num;
+		int trial=0;
 		
-		printf("please enter a string:");
-		while ( (c=getchar())!= '\n')
+		do
 		{
 		
-			if (c>='0' && c<='9')
-				num=num+1;
-		}
-		printf("no, of digits is %i\n", num);
+			printf("guess a number:");
+			scanf("%d", &num);
+			
+			if (num>answer)
+				printf("bigger\n");
+				
+			else if (num<answer)
+				printf("smaller\n");
+				
+			trial = trial+1;
+	    }
+	    
+	    while (num!=answer);
+	    
+
+	    printf("congratulation! trial: %d\n",trial);
 		
-		system("PAUSE");
-		
-		printf("input an string:");
 	
 	
 
