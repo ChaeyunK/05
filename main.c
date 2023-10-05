773d37368da90676f42af8b48f6a0509d2898b2b
ChaeyunK <iehate52@gmail.com>
2023-10-05 16:40:48 +0900

    4

--- main.c
+++ main.c
@@ -4,18 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int input;
 	
-printf("input an integer");
-	scanf("%d", &input);
-	
-	if (input > 0) {
-        printf("positive\n");
-    } else if (input < 0) {
-        printf("negative.\n");
-    } else {
-        printf("zero.\n");
-    }
+	int n,i;
+	int sum;
+
+	printf("input a number");
+	scanf("%d", &i);
+	sum=0;
+
+	for(n=1; n<=i; n++)
+		sum=sum+n;
+	printf("The result is %d\n", sum);
+
+	system("PAUSE");
+
 	return 0;	
 
 	
